36600985afc7c1c1f19db57b5eba4b9001e25577
Rohith Sahani <rohithsahani@Rohiths-MacBook-Air.local>
2026-03-11 20:00:03 +0530

    Fix CORS preflight issue

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.json());
 app.use(bodyParser.json());
