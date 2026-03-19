c23b59a94ce44b3c66be0025674bb22d5125c01e
Rohith Sahani <rohithsahani@Rohiths-MacBook-Air.local>
2026-03-19 21:37:47 +0530

    pay

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://rrmobilesolutions.vercel.app",
+  "https://rrmobilessolutions.vercel.app",
 ];
 
 app.use(
