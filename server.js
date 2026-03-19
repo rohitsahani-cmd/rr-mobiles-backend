ce4225176995a992204081b058bf7ae5345c0d53
Rohith Sahani <rohithsahani@Rohiths-MacBook-Air.local>
2026-03-19 21:14:53 +0530

    cors

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://rrmobilessolutions.vercel.app",
+  "https://rrmobilesolutions.vercel.app",
 ];
 
 app.use(
